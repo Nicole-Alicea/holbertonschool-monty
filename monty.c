411e033c386e7e7b035825245370b6fef1f013b2
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-01-03 21:17:07 -0400

    fixing betty

--- monty.c
+++ monty.c
@@ -15,14 +15,9 @@ int main(int argc, char **argv)
 	FILE *file = fopen(argv[1], "r");
 	stack_t *stack = NULL;
 
-	if (argc != 2)
+	if (argc != 2 || !(file))
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while ((read = getline(&line, &len, file)) != -1)
