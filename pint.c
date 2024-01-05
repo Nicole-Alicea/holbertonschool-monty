decfa453a564d3dbbc02fff19bdd0774efe6da33
GoldenHatchet15 <raphsant20@gmail.com>
2024-01-05 06:44:35 -0400

    C - Stacks, Queues - LIFO, FIFO

--- pint.c
+++ pint.c
@@ -1,18 +1,19 @@
 #include "monty.h"
+extern var_t var;
 
 /**
- * pint - Print value on top of `stack', or exit if stack is empty.
- * @stack: Double pointer to head of stack.
- * @line_number: Line number of current operation.
- *
- * Return: void.
- */
+* pint - Print value on top of `stack', or exit if stack is empty.
+* @stack: Double pointer to head of stack.
+* @line_number: Line number of current operation.
+*
+* Return: void.
+*/
 void pint(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL)
-    {
-        dprintf(STDOUT_FILENO, "L%u: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
+if (var.stack_len == 0)
+{
+dprintf(STDOUT_FILENO, "L%u: can't pint, stack empty\n", line_number);
+exit(EXIT_FAILURE);
+}
+printf("%d\n", (*stack)->n);
 }
