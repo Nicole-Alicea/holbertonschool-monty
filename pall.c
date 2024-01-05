361d74ee4116f227423fcaf6edcb2581e014e105
GoldenHatchet15 <raphsant20@gmail.com>
2024-01-05 05:55:47 -0400

    C - Stacks, Queues - LIFO, FIFO

--- pall.c
+++ pall.c
@@ -1,19 +1,21 @@
 #include "monty.h"
 
 /**
- * pall - This function prints all the values to the stack
- * @stack: Double pointer to the linked list stack
- * @line_number: File's line number
- *
- * Return: void
- */
+* pall - This function prints all the values to the stack
+* @stack: Double pointer to the linked list stack
+* @line_number: File's line number
+*
+* Return: void
+*/
 
-void pall(stack_t **stack, unsigned int line_number) {
-    stack_t *current = *stack;
-    (void) line_number; /*To avoid unused variable warning*/
+void pall(stack_t **stack, unsigned int line_number)
+{
+stack_t *current = *stack;
+(void) line_number; /*To avoid unused variable warning*/
 
-    while (current != NULL) {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+}
 }
