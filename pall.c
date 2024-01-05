becd112dd0d4e0bdc2141f41e0b200282c2f0e9b
GoldenHatchet15 <raphsant20@gmail.com>
2024-01-05 05:17:08 -0400

    C - Stacks, Queues - LIFO, FIFO

--- pall.c
+++ pall.c
@@ -8,19 +8,12 @@
  * Return: void
  */
 
-void pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *head = *stack;
-	(void)line_number;
+void pall(stack_t **stack, unsigned int line_number) {
+    stack_t *current = *stack;
+    (void) line_number; /*To avoid unused variable warning*/
 
-	if (head == NULL)
-	{
-		return;
-	}
-
-	while (head)
-	{
-		printf("%d\n", head->n);
-		head = head->next;
-	}
+    while (current != NULL) {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
 }
